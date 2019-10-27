116e0357457fcff7bb7f45326f394f942f3466ee
wmc2tokyo <wmcooper2@yahoo.com>
2019-10-27 19:21:33 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import HomepageImage from './components/HomepageImage'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomepageImage />
+        <p>
+          First react website.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
